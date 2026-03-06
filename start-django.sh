aa3a7af981eb1af929a3d44c43e4ff5caacd9770
Pressense Fasani <camillefasani@gmail.com>
2026-03-06 11:09:50 +0100

    fix: update start-django.sh to support staging environment for gunicorn

--- start-django.sh
+++ start-django.sh
@@ -2,8 +2,8 @@
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate
 
-if [[ "$ENV_STATE" == "production" ]]; then
-    poetry run gunicorn pause_empathique.wsgi:application --forwarded-allow-ips -"*"
+if [[ "$ENV_STATE" == "production" ]] || [[ "$ENV_STATE" == "staging" ]]; then
+    poetry run gunicorn pause_empathique.wsgi:application --bind "0.0.0.0:${PORT:-8000}" --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
